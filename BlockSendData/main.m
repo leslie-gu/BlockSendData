17f424b436507f1c06ef32defbe94c50b8e4289f
leslie <aaaaa@localhost.(none)>
2012-12-18 22:40:02 +0800

    Initial Commit

---
+++ BlockSendData/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  BlockSendData
+//
+//  Created by leslie on 12-12-18.
+//  Copyright (c) 2012年 leslie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
